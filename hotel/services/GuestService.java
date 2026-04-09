72540cc7643b25a0656cd88a49897b822a8c7cb6
Mennat-Allah <memad2810@gmail.com>
2026-04-09 12:15:36 +0200

    Create GuestService.java
    implemented the guest service of the program

---
+++ hotel/services/GuestService.java
@@ -0,0 +1,102 @@
+package services;
+import hotel.database.HotelDatabase;
+import hotel.enums.PaymentMethod;
+import hotel.models.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+public class GuestService {
+    private final RoomService roomService;
+    private final ReservationService reservationService;
+    private final InvoiceService invoiceService;
+
+    public GuestService(RoomService roomService, ReservationService reservationService, InvoiceService invoiceService)
+    {
+        this.roomService = roomService;
+        this.reservationService = reservationService;
+        this.invoiceService = invoiceService;
+    }
+    public List<Room>viewAvailableRooms()
+    {
+        return roomService.getAvailableRooms();
+    }
+    public List<Room>viewAvailableRoomsByType(String typeName)
+    {
+        return roomService.getAvailableRoomsByType(typeName);
+    }
+    public List<Room>viewAvailableRoomsWithInBudget(double maxBudget)
+    {
+        return roomService.getAvailableRoomsWithInBudget(maxBudget);
+    }
+    public Reservation makeReservation(Guest guest,String roomId,LocalDate checkIn,LocalDate checkOut)
+    {
+        return reservationService.makeReservation(guest.getGuestId(),roomId,checkIn,checkOut);
+    }
+    public List<Reservation>viewMyReservations(Guest guest)
+    {
+        return reservationService.getReservationsByGuest(guest.getGuestId());
+    }
+    public List<Reservation>viewMyActiveReservations(Guest guest)
+    {
+        return reservationService.getReservationsByGuest(guest.getGuestId());
+    }
+    public boolean cancelReservation(Guest guest,String reservationId)
+    {
+        return reservationService.cancelReservation(reservationId,guest.getGuestId());
+    }
+    public Invoice checkoutWithBalance(Guest guest,String reservationId)
+    {
+        Invoice invoice=invoiceService.findByReservationId(reservationId);
+        if(invoice==null)
+        {
+            throw new IllegalArgumentException("No invoice found for reservation:"+reservationId);
+        }
+        invoiceService.payWithBalance(invoice.getInvoiceId(),guest);
+        return invoice;
+    }
+    public Invoice checkoutWithExternalPayment(String reservationId,double amountTendered,PaymentMethod paymentMethod)
+    {
+        Invoice invoice=invoiceService.findByReservationId(reservationId);
+        if(invoice==null)
+        {
+            throw new IllegalArgumentException("No invoice found for reservation:"+reservationId);
+        }
+        invoiceService.payWithExternal(invoice.getInvoiceId(),amountTendered,paymentMethod);
+        return invoice;
+    }
+    public List<Invoice>viewMyInvoices(Guest guest)
+    {
+        return invoiceService.getInvoicesByGuest(guest.getGuestId());
+    }
+    public void updateAddress(Guest guest, String newAddress)
+    {
+        guest.setAddress(newAddress);
+    }
+    public void updateRoomPreferences(Guest guest, RoomPreferences preferences)
+    {
+        guest.setRoomPreferences(preferences);
+    }
+    public void depositBalance(Guest guest, double amount)
+    {
+        guest.deposit(amount);
+    }
+    public Guest findGuestById(String guestId)
+    {
+
+        for (int i=0;i<HotelDatabase.guests.size();i++)
+        {
+            Guest g =HotelDatabase.guests.get(i);
+            if(g.getGuestId().equals(guestId))
+            {
+                return g;
+            }
+        }
+        return null;
+    }
+    public List<Guest> getAllGuests()
+    {
+        return HotelDatabase.guests;
+    }
+    
+}
