b795966383d1f3315c258eb7dc7c8fae6b607b7b
Mennat-Allah <memad2810@gmail.com>
2026-04-09 15:02:47 +0200

    Create AdminService.java
    implement AdminService

---
+++ hotel/services/AdminService.java
@@ -0,0 +1,184 @@
+package hotel.services;
+import hotel.database.HotelDatabase;
+import hotel.models.*;
+
+import java.util.List;
+public class AdminService {
+    private final RoomService roomService;
+    private final GuestService guestService;
+    private final InvoiceService invoiceService;
+
+    public AdminService(RoomService roomService, GuestService guestService, InvoiceService invoiceService) {
+        this.roomService = roomService;
+        this.guestService = guestService;
+        this.invoiceService = invoiceService;
+    }
+    public void addRoom(Room room)
+    {
+        roomService.add(room);
+    }
+    public boolean updateRoom(Room room)
+    {
+        return roomService.update(room);
+    }
+    public boolean deleteRoom(String roomId)
+    {
+        return roomService.delete(roomId);
+    }
+    public Room getRoom(String roomId)
+    {
+        return roomService.findById(roomId);
+    }
+    public List <Room>getAllRooms()
+    {
+        return roomService.getAllRooms();
+    }
+
+
+    public void addRoomType(RoomType rt)
+    {
+        roomService.addRoomType(rt);
+    }
+    public boolean updateRoomType(RoomType rt)
+    {
+        return roomService.updateRoomType(rt);
+    }
+    public boolean deleteRoomType(String typeId)
+    {
+        return roomService.deleteRoomType(typeId);
+    }
+    public RoomType getRoomType(String typeId)
+    {
+        return roomService.findRoomTypeById(typeId);
+    }
+    public List <RoomType>getAllRoomTypes()
+    {
+        return roomService.getAllRoomTypes();
+    }
+
+
+    public void addAmenity(Amenity a)
+    {
+        roomService.addAmenity(a);
+    }
+    public boolean updateAmenity(Amenity a)
+    {
+        return roomService.updateAmenity(a);
+    }
+    public boolean deleteAmenity(String amenityId)
+    {
+        return roomService.deleteAmenity(amenityId);
+    }
+    public Amenity getAmenity(String amenityId)
+    {
+        return roomService.findAmenityById(amenityId);
+    }
+    public List <Aminety>getAllAmenities()
+    {
+        return roomService.getAllAmenities();
+    }
+
+    public List <Guest>getAllGuests()
+    {
+        return guestService.getAllGuests();
+    }
+    public Guest getGuest(String guestId)
+    {
+        return guestService.findGuestById(guestId);
+    }
+    public boolean removeGuest(String guestId)
+    {
+        for(int i=0;i<HotelDatabase.guests.size();i++)
+        {
+            if(HotelDatabase.guests.get(i).getGuestId().equals(guestId))
+            {
+                HotelDatabase.guests.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public List <Staff>getAllStaff()
+    {
+        return HotelDatabase.staffMembers;
+    }
+
+    public Staff getStaff(String staffId)
+    {
+        for(int i=0;i<HotelDatabase.staffMembers.size();i++)
+        {
+            Staff s =HotelDatabase.staffMembers.get(i);
+            if(s.getStaffId().equals(staffId))
+            {
+
+                return s;
+            }
+        }
+        return null;
+    }
+    public boolean removeStaff(String staffId)
+    {
+        boolean removed=false;
+        for(int i=0;i<HotelDatabase.staffMembers.size();i++)
+        {
+            if(HotelDatabase.staffMembers.get(i).getStaffId().equals(staffId))
+            {
+                HotelDatabase.staffMembers.remove(i);
+                i--;
+                removed = true;
+            }
+        }
+        return removed;
+    }
+    public List<Reservation>getAllReservations()
+    {
+        return HotelDatabase.reservation;
+    }
+
+
+    public List<Invoice>getAllInvoices()
+    {
+        return invoiceService.getAllInvoices();
+    }
+    public double getTotalRevenue()
+    {
+        double total=0;
+        for(int i=0;i<HotelDatabase.invoices.size();i++)
+        {
+            Invoice invoice =HotelDatabase.invoices.get(i);
+            if(invoice.isPaid())
+            {
+                total+=invoice.getAmountPaid();
+            }
+        }
+        return total;
+    }
+    public long getOccupiedRoomCount()
+    {
+        long count=0;
+        for(int i=0;i<HotelDatabase.rooms.size();i++)
+        {
+            Room room =HotelDatabase.rooms.get(i);
+            if(!room.isAvailable())
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    public long getAvailableRoomCount()
+    {
+        long count=0;
+        for(int i=0;i<HotelDatabase.rooms.size();i++)
+        {
+            Room room =HotelDatabase.rooms.get(i);
+            if(room.isAvailable())
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
