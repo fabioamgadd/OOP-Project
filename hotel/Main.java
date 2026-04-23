3adc028e0ef292c2dadbe5a5c1ba48bbb58da5d5
fabioamgadd <fabio.amgad333@gmail.com>
2026-04-23 18:57:28 +0200

    Update Main.java

--- hotel/Main.java
+++ hotel/Main.java
@@ -15,10 +15,7 @@ public class Main {
         System.out.println("Importing database.");
         int guestCount = HotelDatabase.guests.size();
         int roomCount = HotelDatabase.rooms.size();
-        System.out.printf("  Loaded %d guests, %d rooms, %d room types, %d amenities.%n",
-                guestCount, roomCount,
-                HotelDatabase.roomTypes.size(),
-                HotelDatabase.amenities.size());
+        System.out.printf("  Loaded %d guests, %d rooms, %d room types, %d amenities.%n", guestCount, roomCount, HotelDatabase.roomTypes.size(), HotelDatabase.amenities.size());
 
         System.out.println("\nStored accounts:");
         System.out.println("\nStaff:");
@@ -36,8 +33,7 @@ public class Main {
         ReservationService reservationService = new ReservationService(roomService, invoiceService);
         GuestService guestService = new GuestService(roomService, reservationService, invoiceService);
         AdminService adminService = new AdminService(roomService, guestService, invoiceService);
-        ReceptionistService receptionistService = new ReceptionistService(reservationService, roomService,
-                guestService);
+        ReceptionistService receptionistService = new ReceptionistService(reservationService, roomService, guestService);
         AuthService authService = new AuthService();
 
         Scanner scanner = new Scanner(System.in);
