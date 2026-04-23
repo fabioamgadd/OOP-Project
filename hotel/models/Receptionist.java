cf9529e68ba2116ff7f514ea918a4b1792d023a0
Selena <sesosamy12@gmail.com>
2026-04-23 19:03:51 +0200

    Update Receptionist.java

--- hotel/models/Receptionist.java
+++ hotel/models/Receptionist.java
@@ -3,11 +3,18 @@ package hotel.models;
 import hotel.enums.Role;
 
 public class Receptionist extends Staff, Role.RECEPTIONIST {
-    Receptionist (){
-        super();
+     public Receptionist(String staffId1, String username, String password,String dob , int workingHours , Gender gender){
+        super(staffId1,username,password,dob,Role.RECEPTIONIST,workingHours,gender);
+    }
+ublic Receptionist(String username, String password,String dob , int workingHours , Gender gender){
+        super(username,password,dob,Role.RECEPTIONIST,workingHours,gender);
     }
     @Override
     public String getRoleDescription() {
         return "this is the Receptionist ";
+    }
+@Override
+    public String toString() {
+        return ("Receptionist" + super.toString());
     }
 }
