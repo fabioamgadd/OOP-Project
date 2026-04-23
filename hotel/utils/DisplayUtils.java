d88559185bdcd7da06288ae64e95da1bb71133d3
Mennat-Allah <memad2810@gmail.com>
2026-04-23 15:32:30 +0200

    Update DisplayUtils.java
    Update DisplayUtils

--- hotel/utils/DisplayUtils.java
+++ hotel/utils/DisplayUtils.java
@@ -30,7 +30,7 @@ public class DisplayUtils
                 String result =" ";
                 for(int j=0;j<r.getAmenities().size();j++)
                 {
-                    Amenity a =r.getAmenities.get(j);
+                    Amenity a =r.getAmenities().get(j);
                     result += a.getName();
                     if(j<r.getAmenities().size()-1)
                     {
@@ -40,7 +40,7 @@ public class DisplayUtils
                 }
                 amenities=result;
             }
-            System.out.printf("%-12s %-8d %-12s %-12s %-15.2f %s%n","r.getRoomId(),r.getFloorNumber(),r.getRoomType().getTypeName(),r.isAvailable()?"Yes":"No",
+            System.out.printf("%-12s %-8d %-12s %-12s %-15.2f %s%n",r.getRoomId(),r.getFloorNumber(),r.getRoomType().getTypeName(),r.isAvailable()?"Yes":"No",
                     r.getTotalPricePerNight(),amenities);
         }
     }
@@ -54,7 +54,7 @@ public class DisplayUtils
         for(int i=0;i<reservations.size();i++)
         {
             Reservation res = reservations.get(i);
-            System.out.printf("%-18s %-10s %-15s %-15s %-10.2f %s%n",res.getReservationId(), res.getRoomId(), res.CheckInDate(), res.CheckOutDate(), res.getTotalCost(),
+            System.out.printf("%-18s %-10s %-15s %-15s %-10.2f %s%n",res.getReservationId(), res.getRoomId(), res.getCheckInDate(), res.getCheckOutDate(), res.getTotalCost(),
                     res.getStatus());
         }
 
@@ -69,7 +69,7 @@ public class DisplayUtils
         for(int i=0;i<guests.size();i++)
         {
             Guest g = guests.get(i);
-            System.out.printf("%-12s %-15s %-10s %.2f%n",g.getGuestId(), g.getUsename(), g.getGender(), g.getBalance());
+            System.out.printf("%-12s %-15s %-10s %.2f%n",g.getGuestId(), g.getUsername(), g.getGender(), g.getBalance());
         }
 
     }
