2359e2e6654c6e9c0c670490dd7c637174f706c0
Selena <sesosamy12@gmail.com>
2026-04-23 18:54:23 +0200

    Update Admin.java

--- hotel/models/Admin.java
+++ hotel/models/Admin.java
@@ -3,11 +3,18 @@ package hotel.models;
 import hotel.enums.Role;
 
 public class Admin extends Staff, Role.ADMIN {
-    Admin (){
-        super();
+   public Admin(String staffId1, String username, String password,String dob , int workingHours , Gender gender){
+        super(staffId1,username,password,dob,Role.ADMIN,workingHours,gender);
+    }
+ public Admin( String username, String password,String dob , int workingHours , Gender gender){
+        super(username,password,dob,Role.ADMIN,workingHours,gender);
     }
     @Override
     public String getRoleDescription() {
         return "this is the admin";
+    }
+@Override
+    public String toString() {
+        return String.format("Admin{%s}", super.toString());
     }
 }
