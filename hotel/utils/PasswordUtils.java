db1f0e0324bb2e2e6861984e2e1764a8cec53f9a
Selena <sesosamy12@gmail.com>
2026-04-26 22:01:40 +0300

    Update PasswordUtils.java

--- hotel/utils/PasswordUtils.java
+++ hotel/utils/PasswordUtils.java
@@ -1,6 +1,10 @@
 package hotel.utils;
 
 public class PasswordUtils {
+    private PasswordUtils() {}
+    public static String hash(String plainPassword) {
+        return plainPassword;
+    }
     public static boolean matches(String input, String stored) {
         return input.equals(stored);
     }
